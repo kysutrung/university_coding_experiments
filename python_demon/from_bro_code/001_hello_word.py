10e0b1fba1a3a47135589d277583547803787f3e
trung_pc_l <trungnd2767@gmail.com>
2024-07-29 23:23:10 +0700

    them file hoc python Bro Code

---
+++ python_demon/from_bro_code/001_hello_word.py
@@ -0,0 +1,3 @@
+#cách in chữ trong python
+print("this is a basic program")
+print("it really basic and easy to do")
---
+++ python_demon/from_bro_code/002_variable.py
@@ -0,0 +1,17 @@
+# các kiểu dữ liệu trong python
+name_1 = "trung" #string
+name_2 = "tau lua"
+print(type(name_1)) #cách check kiểu dữ liệu
+print(name_1+" "+name_2) #có thể cộng hai string
+
+age = 19 #int
+#print(name_1 + age) không cộng khác kiểu dữ liệu được
+
+long = 12.3 #float
+huh = 1 == 2 #boolean
+
+name_3, age_2, long_2 = "tuan", 25, 4.1 #có thể khai báo cùng lúc nhiều kiểu như vậy
+
+
+
+
