d730e1f98340b12ec7c125bd913b17ea31f4697d
trung_pc_l <trungnd2767@gmail.com>
2024-08-05 21:34:24 +0700

    update

--- python_demon/self_learning_py/ex_2.py
+++
@@ -1,3 +0,0 @@
-"""Bài tập 2: Tìm số dài nhất trong danh sách các số nguyên
-Yêu cầu: Viết hàm tim_so_dai_nhat(list_a) để tìm số có nhiều chữ số
-nhất trong danh sách các số nguyên list_a."""
---
+++ python_demon/self_learning_py/which_number_longer.py
@@ -0,0 +1,15 @@
+"""Bài tập 2: Tìm số dài nhất trong danh sách các số nguyên
+Yêu cầu: Viết hàm tim_so_dai_nhat(list_a) để tìm số có nhiều chữ số
+nhất trong danh sách các số nguyên list_a."""
+
+list_b = [1, 19999923, 23, 6542, 828262]
+
+def tim_so_dai_hon (list_a):
+    max_val = 0
+    max_index = 0
+    for i in range (0, len(list_a)):
+        if max_val < len(str(list_a[i])):
+            max_val = len(str(list_a[i]))
+            max_index = i
+    print(list_a[max_index])
+
