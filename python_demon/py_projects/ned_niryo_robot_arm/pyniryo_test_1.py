a9f654a15e88ef8d08237747b6754ea908c74630
trung_pc_l <trungnd2767@gmail.com>
2024-08-06 16:51:32 +0700

    tai len code test niryo ned

--- python_demon/py_projects/ned_niryo_robot_arm/pyniryo_test_1.py
+++ python_demon/py_projects/ned_niryo_robot_arm/pyniryo_test_1.py
@@ -10,8 +10,10 @@ for i in range(0,3):
     robot.move_joints(0.5, 0.0, 0.0, 0.0, 0.0, 0.0)
     robot.move_joints(0.0, 0.5, 0.0, 0.0, 0.0, 0.0)
     robot.move_joints(-0.5, 0.0, 0.0, 0.0, 0.0, 0.0)
-robot.move_joints(-0.002, 0.493, -1.327, 0.011, -0.089, -0.007)
-robot.wait(5)
+#robot.move_pose(0.21, 0, 0.4, 0, 1.57, 0) #vị trí quan sát bàn gắp
+robot.move_joints(-0.002, 0.493, -1.327, 0.011, -0.089, -0.007) #vị trí nghỉ đẹp
+robot.wait(3)
+
 # Turn learning mode ON
 robot.set_learning_mode(True)
 # Stop TCP connection
