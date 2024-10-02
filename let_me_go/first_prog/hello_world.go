33926632ee8f258189d087a3b734f18f13cee831
trung_pc_l <trungnd2767@gmail.com>
2024-10-02 23:55:35 +0700

    hoc mot so kien thuc co ban ve golang

--- let_me_go/first_prog/hello_world.go
+++ let_me_go/first_prog/hello_world.go
@@ -2,7 +2,35 @@ package main
 
 import "fmt" // đây là thư viện format 
 
+var num_a int = 10
+
+var(
+	num_c int = 30
+	str_a string = "meo meo"
+)
+
+func tinh_tong(n int, m int) int {
+	return n + m
+}
+
+func so_sanh(n int, m int) {
+	if n < m {
+		fmt.Print("less than")
+	}
+	if n > m {
+		fmt.Print("more than")
+	}
+	if n == m {
+		fmt.Print("equal")
+	}
+}
+
+
 func main() {
-    fmt.Print("Hello World!!!")
-	fmt.Print(" Xin chao co chu xin chao cac ban!!!")
+	//num_b := 20
+	//khai báo kiểu rút gọn này chỉ dùng trong hàm main
+
+    fmt.Print(tinh_tong(num_a, num_c))
+	fmt.Println(" Xin chao co chu xin chao cac ban!!!")
+	so_sanh(num_a, num_c)
 }
